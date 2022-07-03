0638e7b71642c56a8b3a57441319543f8f478a82
MrunaliDeore <mrunalideoreohk1@gmail.com>
2022-07-03 10:22:58 +0530

    Modified: part time employee wage UC3

--- ParttimeEmpWageUC3.java
+++ ParttimeEmpWageUC3.java
@@ -1,25 +1,28 @@
-//UC1
-//Check Employee is Present or Absent
-//- Use ((RANDOM)) for Attendance Check
+//UC3
+
 
 package Three;
 
-public class CalculateWageUC2 {
+public class ParttimeEmpWageUC3 {
 	
 	public static void main (String[] args){
 	
-		int FULL_TIME=1;
+		int PART_TIME=1;
+		int FULL_TIME=2;
 		int PER_HOUR=20;
 		
 		int emphrs=0;
 		int empwage=0;
 		
-		double empCheck=Math.floor(Math.random()*10%2);
+		double empCheck=Math.floor(Math.random()*10%3);
 		
-				if(empCheck==FULL_TIME)
+				if(empCheck==PART_TIME)
+					emphrs=4;
+				else if(empCheck==FULL_TIME)
 					emphrs=8;
 				else
 					emphrs=0;
+				
 				empwage=emphrs*PER_HOUR;
 					
 				System.out.println("Employee Wage :"+empwage);
