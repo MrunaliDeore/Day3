2bbe5bbbac4e68537c6a80c02f69d778ebe407ca
MrunaliDeore <mrunalideoreohk1@gmail.com>
2022-07-03 10:13:23 +0530

    Modified: calculate wage UC2

--- CalculateWageUC2.java
+++ CalculateWageUC2.java
@@ -4,16 +4,24 @@
 
 package Three;
 
-public class EmployeeWaageUC1 {
+public class CalculateWageUC2 {
 	
 	public static void main (String[] args){
 	
 		int FULL_TIME=1;
+		int PER_HOUR=20;
+		
+		int emphrs=0;
+		int empwage=0;
+		
 		double empCheck=Math.floor(Math.random()*10%2);
 		
 				if(empCheck==FULL_TIME)
-					System.out.println("Employee is present");
+					emphrs=8;
 				else
-					System.out.println("Employee is not presenr");
+					emphrs=0;
+				empwage=emphrs*PER_HOUR;
+					
+				System.out.println("Employee Wage :"+empwage);
 	}
 }
